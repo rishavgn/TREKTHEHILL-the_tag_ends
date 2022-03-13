061a71fa9f78c40873955dcb8fa8ece5f0e02f47
rishavgn <77581771+rishavgn@users.noreply.github.com>
2022-03-13 16:48:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -40,15 +40,7 @@ app.use("/api/users", usersRouter);
 app.use("/api/contacts", contactRouter);
 app.use("/api/patients", patientRouter);
 
-/* //Serve static assets if in production
-if (process.env.NODE_ENV = "production") {
-    app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
- */
 app.use("/api/payment", paymentRoutes);
 
 const port = process.env.PORT || 5000;
